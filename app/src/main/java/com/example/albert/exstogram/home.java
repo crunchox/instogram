5effa10aac92b7fd8b5390f17463986435f64fc9
Albert <firstandlastgame@gmail.com>
2017-09-18 22:04:43 +0700

    "Add comment at home"

--- app/src/main/java/com/example/albert/exstogram/home.java
+++ app/src/main/java/com/example/albert/exstogram/home.java
@@ -1,5 +1,5 @@
 package com.example.albert.exstogram;
-
+//ini comment buat second commit
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
